636e1905bcf604071d7ccbcbd1853e7bd3a9a3a5
HannahMidd <hmiddleton1017@gmail.com>
2023-06-13 20:11:50 -0400

    adding questions to generator

--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,38 @@
 // TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Array of questions for user input
+
+const questions = [
+  {
+    type: "input",
+    name: "github name",
+    message: "What is your Github username?",
+  },
+
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email?",
+  },
+
+  {
+    type: "input",
+    name: "title",
+    message: "What is the name of this project?",
+  },
+
+  {
+    type: "input",
+    name: "description",
+    message: "What is the desciption of your project?",
+  },
+
+  {
+    type: "input",
+    name: "installation instructions",
+    message: "Provide installation instructions. If none, write N/A. ",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
